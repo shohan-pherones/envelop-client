9ecb4d23b6e5957819e3f37106c59c13e17d5947
shohan-pherones <shohan.sub.56@gmail.com>
2023-03-08 23:38:43 +0600

    login and register page added

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,53 @@
+import { useState } from "react";
+import FormControl from "../components/FormControl";
+import SectionTitle from "../components/SectionTitle";
+import Button from "../components/Button";
+
 const Login = () => {
-  return <div>Login</div>;
+  const [formFields, setFormFeilds] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+
+    console.log(formFields);
+
+    // clear state
+    setFormFeilds({
+      email: "",
+      password: "",
+    });
+  };
+
+  return (
+    <div className="login flex flex-col justify-center items-center mt-20">
+      <form onSubmit={handleLogin} className="flex flex-col gap-5">
+        <SectionTitle title={"Login..."} />
+
+        <FormControl
+          label="email"
+          labelInner="Email Address"
+          inputType="email"
+          placeholder="Write your email"
+          formFields={formFields}
+          setFormFeilds={setFormFeilds}
+        />
+
+        <FormControl
+          label="password"
+          labelInner="Password"
+          inputType="password"
+          placeholder="Write your password"
+          formFields={formFields}
+          setFormFeilds={setFormFeilds}
+        />
+
+        <Button text="Login" submit />
+      </form>
+    </div>
+  );
 };
 
 export default Login;
