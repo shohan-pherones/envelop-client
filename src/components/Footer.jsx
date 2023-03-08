1cb0cff571a3d2d00cdb5db0f1e792583febc277
shohan-pherones <shohan.sub.56@gmail.com>
2023-03-08 22:50:47 +0600

    navbar added

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="text-center py-20 text-gray-500">
+      <p>&copy; {new Date().getFullYear()} Envelop. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
